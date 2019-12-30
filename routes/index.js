8a7a8b11e6baedb6c655fde833b73a7966d34ef5
ridwan <rdwn.dev@gmail.com>
2019-12-31 01:20:00 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,109 @@
+var express = require("express");
+var router = express.Router();
+var xlsxParser = require("xls-parser");
+const multer = require("multer");
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  }
+});
+
+var upload = multer({ storage: storage });
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  // console.log(req);
+  // xlsxParser.onFileSelection(req.file).then(data => {
+  //   var parsedData = data;
+  // });
+  res.render("index", { title: "Express" });
+});
+
+router.post("/upload", upload.single("myFile"), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  // res.send(file);
+  node_xj = require("xls-to-json");
+  node_xj(
+    {
+      input: file.path, // input xls
+      output: "output.json" // output json
+      // sheet: "sheetname", // specific sheetname
+      // rowsToSkip: 5 // number of rows to skip at the top of the sheet; defaults to 0
+    },
+    function(err, result) {
+      if (err) {
+        console.error(err);
+      } else {
+        var poslaju = [];
+        var u = 0;
+        const alasql = require("alasql");
+        var date = new Date();
+
+        for (var i of result) {
+          u++;
+          var address = i["MAILING ADDRESS"].split(",");
+          // console.log(u);
+          poslaju.push({
+            NO: u,
+            "Parcel Content": "Fashion & Apparel - Sports",
+            "Content Description":
+              i["TICKET VARIANT"] + " " + i["T-SHIRT SIZE"] || "",
+            "Value of goods(RM)": 1,
+            "Weight(Kg)*": "0.5",
+            "Send Method*": "drop off",
+            "Send Date*": "2019-12-30",
+            "Sender Name": "Oh My Run Asia",
+            "Sender Company": "",
+            "Sender Phone*": "0173112801",
+            "Sender Email*": "support@ohmyrun.com",
+            "Sender Address Line 1*": "F111",
+            "Sender Address Line 2*": "Apartment Saujana",
+            "Sender Address Line 3": "Jalan PJU 10/1c",
+            "Sender Address Line 4": "Damansara Damai",
+            "Sender Postcode*": "47820",
+            "Sender City": "Petaling Jaya",
+            "Sender State": "Selangor",
+            "Receiver Name": i["FULL NAME"],
+            "Receiver Company": "",
+            "Receiver Contact*": i["MOBILE NUMBER"],
+            "Receiver Email": i["E-MAIL ADDRESS"],
+            "Receiver Address Line 1*": address[0] || "",
+            "Receiver Address Line 2": address[1] || "",
+            "Receiver Address Line 3": address[2] || "",
+            "Receiver Address Line 4": address[3] || "",
+            "Receiver Postcode*": i["POSTCODE/ZIPCODE"],
+            "Receiver City": address[3] || "",
+            "Receiver State": address[4] || ""
+          });
+          var d = result.length - 1;
+          if (u === d) {
+            console.log(d);
+            var json2xls = require("json2xls");
+            var xls = json2xls(poslaju);
+            // res.send(xls);
+            res.xls("data.xlsx", poslaju);
+            // return res.json(poslaju);
+            // return alasql(`SELECT * INTO XLSX("test411.xlsx",{headers:true})`, [
+            //   poslaju
+            // ]);
+          }
+        }
+        // console.log(result);
+
+        // return res.json(result);
+      }
+    }
+  );
+});
+
+module.exports = router;
