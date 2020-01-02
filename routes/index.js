36f634709b0f0c53376d5e717b0eb807e35e0a39
ridwan <rdwn.dev@gmail.com>
2020-01-03 01:27:48 +0800

    added justrunlah options

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,8 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/upload", upload.single("myFile"), (req, res, next) => {
+  const filename = req.body.filename;
+  const source = req.body.source;
   const file = req.file;
   if (!file) {
     const error = new Error("Please upload a file");
@@ -51,79 +53,172 @@ router.post("/upload", upload.single("myFile"), (req, res, next) => {
           var u = 0;
           const alasql = require("alasql");
           var date = new Date();
-
-          for (var i of result) {
-            u++;
-            var address = i["MAILING ADDRESS"].split(",");
-            if (address[3]) {
-              address[3].replace(/[0-9]/g, "");
-            }
-            for (var k in address) {
-              address[k] = address[k].toUpperCase();
-            }
-            var poscode = parseInt(
-              i["POSTCODE/ZIPCODE"].replace(/[^0-9\.]/g, ""),
-              10
-            );
-            var cityAndState = JSON.parse(await getCity(poscode));
-            var city, state;
-
-            if (cityAndState.length > 0) {
-              city = cityAndState[0].city.toUpperCase();
-              state = cityAndState[0].state;
-            }
-            // console.log(u);
-            poslaju.push({
-              No: u,
-              "Parcel Content": "Fashion & Apparel - Sports",
-              "Content Description":
-                i["TICKET VARIANT"] + " " + i["T-SHIRT SIZE"] || "",
-              "Value of goods(RM)": 1,
-              "Weight(Kg)*": "0.2",
-              "Send Method*": "drop off",
-              "Send Date*": moment(date).format("DD/MM/YYYY"),
-              "Sender Name": "Oh My Run Asia",
-              "Sender Company": "",
-              "Sender Phone*": "0173112801",
-              "Sender Email*": "support@ohmyrun.com",
-              "Sender Address Line 1*": "F111, Block F3",
-              "Sender Address Line 2*": "Apartment Saujana",
-              "Sender Address Line 3": "Jalan PJU 10/1c",
-              "Sender Address Line 4": "Damansara Damai",
-              "Sender Postcode*": "47820",
-              "Sender City": "Petaling Jaya",
-              "Sender State": "SELANGOR",
-              "Receiver Name": i["FULL NAME"],
-              "Receiver Company": "",
-              "Receiver Contact*": i["MOBILE NUMBER"],
-              "Receiver Email": i["E-MAIL ADDRESS"],
-              "Receiver Address Line 1*": address[0] || "",
-              "Receiver Address Line 2": address[1] || "",
-              "Receiver Address Line 3": address[2] || "",
-              "Receiver Address Line 4": address[3] || "",
-              "Receiver Postcode*": poscode,
-              "Receiver City": city || "",
-              "Receiver State": state || ""
-            });
-            var d = result.length - 1;
-            if (u === d) {
-              console.log(d);
-              var json2xls = require("json2xls");
-              // var xls = json2xls(poslaju);
-              // res.send(xls);
-
-              res.xls(
-                "data-" +
-                  moment(date)
-                    .tz("Asia/Kuala_Lumpur")
-                    .format("DD-MM-YYYY HH:mm:ss") +
-                  ".xlsx",
-                poslaju
+          console.log(source);
+          if (source == "ticket2u") {
+            for (var i of result) {
+              u++;
+              var address = i["MAILING ADDRESS"].split(",");
+              if (address[3]) {
+                address[3].replace(/[0-9]/g, "");
+              }
+              for (var k in address) {
+                address[k] = address[k].toUpperCase();
+              }
+              var poscode = parseInt(
+                i["POSTCODE/ZIPCODE"].replace(/[^0-9\.]/g, ""),
+                10
               );
-              // return res.json(poslaju);
-              // return alasql(`SELECT * INTO XLSX("test411.xlsx",{headers:true})`, [
-              //   poslaju
-              // ]);
+              var cityAndState = JSON.parse(await getCity(poscode));
+              var city, state;
+
+              if (cityAndState.length > 0) {
+                city = cityAndState[0].city.toUpperCase();
+                state = cityAndState[0].state;
+              }
+              // console.log(u);
+              poslaju.push({
+                No: u,
+                "Parcel Content": "Fashion & Apparel - Sports",
+                "Content Description":
+                  i["TICKET VARIANT"] + " " + i["T-SHIRT SIZE"] || "",
+                "Value of goods(RM)": 1,
+                "Weight(Kg)*": "0.2",
+                "Send Method*": "drop off",
+                "Send Date*": moment(date).format("DD/MM/YYYY"),
+                "Sender Name": "Oh My Run Asia",
+                "Sender Company": "",
+                "Sender Phone*": "0173112801",
+                "Sender Email*": "support@ohmyrun.com",
+                "Sender Address Line 1*": "F111, Block F3",
+                "Sender Address Line 2*": "Apartment Saujana",
+                "Sender Address Line 3": "Jalan PJU 10/1c",
+                "Sender Address Line 4": "Damansara Damai",
+                "Sender Postcode*": "47820",
+                "Sender City": "Petaling Jaya",
+                "Sender State": "SELANGOR",
+                "Receiver Name": i["FULL NAME"],
+                "Receiver Company": "",
+                "Receiver Contact*": i["MOBILE NUMBER"],
+                "Receiver Email": i["E-MAIL ADDRESS"],
+                "Receiver Address Line 1*": address[0] || "",
+                "Receiver Address Line 2": address[1] || "",
+                "Receiver Address Line 3": address[2] || "",
+                "Receiver Address Line 4": address[3] || "",
+                "Receiver Postcode*": poscode,
+                "Receiver City": city || "",
+                "Receiver State": state || ""
+              });
+              var d = result.length - 1;
+              if (u === d) {
+                var json2xls = require("json2xls");
+
+                // return res.json(poslaju);
+                res.xls(
+                  filename +
+                    "-" +
+                    moment(date)
+                      .tz("Asia/Kuala_Lumpur")
+                      .format("DD-MM-YYYY HH:mm:ss") +
+                    ".xlsx",
+                  poslaju
+                );
+
+                // return alasql(`SELECT * INTO XLSX("test411.xlsx",{headers:true})`, [
+                //   poslaju
+                // ]);
+              }
+            }
+          }
+          if (source == "justrun") {
+            for (var i of result) {
+              u++;
+              var address = i["Mailing address"];
+
+              if (address.indexOf(",") > -1) {
+                address = address.replace(/\n/g, ",");
+                address = address.split(",");
+              } else {
+                console.log("##############");
+                console.log(address);
+                console.log("-------------");
+                address = address.replace(/\n/g, ",");
+                console.log(address);
+                console.log("##############");
+                address = address.split(",");
+                console.log(address);
+              }
+
+              // if (address[3]) {
+              //   address[3].replace(/[0-9]/g, "");
+              // }
+              for (var k in address) {
+                address[k] = address[k].toUpperCase();
+              }
+              // var poscode = parseInt(
+              //   i["POSTCODE/ZIPCODE"].replace(/[^0-9\.]/g, ""),
+              //   10
+              // );
+              // var cityAndState = JSON.parse(await getCity(poscode));
+              // var city, state;
+
+              // if (cityAndState.length > 0) {
+              //   city = cityAndState[0].city.toUpperCase();
+              //   state = cityAndState[0].state;
+              // }
+              // console.log(u);
+              poslaju.push({
+                No: u,
+                "Parcel Content": "Fashion & Apparel - Sports",
+                "Content Description":
+                  i["Entitlement (label)"].slice(0, -5) +
+                    " " +
+                    i["T-Shirt Size (label)"] || "",
+                "Value of goods(RM)": 1,
+                "Weight(Kg)*": "0.2",
+                "Send Method*": "drop off",
+                "Send Date*": moment(date).format("DD/MM/YYYY"),
+                "Sender Name": "Oh My Run Asia",
+                "Sender Company": "",
+                "Sender Phone*": "0173112801",
+                "Sender Email*": "support@ohmyrun.com",
+                "Sender Address Line 1*": "F111, Block F3",
+                "Sender Address Line 2*": "Apartment Saujana",
+                "Sender Address Line 3": "Jalan PJU 10/1c",
+                "Sender Address Line 4": "Damansara Damai",
+                "Sender Postcode*": "47820",
+                "Sender City": "Petaling Jaya",
+                "Sender State": "SELANGOR",
+                "Receiver Name": i["First Name"] + i["Last Name"],
+                "Receiver Company": "",
+                "Receiver Contact*": i["Phone number"],
+                "Receiver Email": i["Email Address"],
+                "Receiver Address Line 1*": address[0] || "",
+                "Receiver Address Line 2": address[1] || "",
+                "Receiver Address Line 3": address[2] || "",
+                "Receiver Address Line 4": address[3] || "",
+                "Receiver Postcode*": "",
+                "Receiver City": address[4] || "",
+                "Receiver State": address[5] || ""
+              });
+              var d = result.length - 1;
+              if (u === d) {
+                var json2xls = require("json2xls");
+
+                return res.json(poslaju);
+                res.xls(
+                  filename +
+                    "-" +
+                    moment(date)
+                      .tz("Asia/Kuala_Lumpur")
+                      .format("DD-MM-YYYY HH:mm:ss") +
+                    ".xlsx",
+                  poslaju
+                );
+
+                // return alasql(`SELECT * INTO XLSX("test411.xlsx",{headers:true})`, [
+                //   poslaju
+                // ]);
+              }
             }
           }
         }
